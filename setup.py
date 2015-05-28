51dd697ae9887a0f840bb7a2bf4bcdc26183c7d3
Nicholas Serra <nicks@ydekproductions.com>
2015-05-28 18:08:41 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sprintly_commit_parser',
-    version='0.1.0',
+    version='0.2.0',
     description='Utility for parsing commits for Sprintly commands.',
     long_description='',
     keywords='sprintly, commit, parser',
