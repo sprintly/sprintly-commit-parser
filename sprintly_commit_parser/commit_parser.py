be18dce9afdd609fefd84ede2b410147b2c85b41
Joe Stump <joe@sprint.ly>
2017-09-29 12:03:38 -0700

    Add the ability to reference tickets with a product ID (e.g. #111:24 for product_id=111, item_number=24). Introducing a colon forced me to remove support for some of the less clear options as well.

--- sprintly_commit_parser/commit_parser.py
+++ sprintly_commit_parser/commit_parser.py
@@ -2,23 +2,40 @@ import re
 
 
 TICKET_PREFIX = '(?:#|(?:ticket|issue|item|defect|bug)[: ]?)'
-TICKET_RE = re.compile(TICKET_PREFIX + '([0-9]+)', re.I | re.UNICODE)
+TICKET_ID = '(([0-9]+:)?([0-9]+))'
+TICKET_RE = re.compile(TICKET_PREFIX + TICKET_ID, re.I | re.UNICODE)
 
 
 def items_mentioned(string):
     if string is None or string.strip() == "":
         return []
-    return [int(x) for x in TICKET_RE.findall(string)]
+
+    items = []
+    for _ in TICKET_RE.findall(string):
+        x = _[0]
+        if ':' in x:
+            product_id, item_number = x.split(':')
+            ticket_id = {
+                'product_id': int(product_id),
+                'item_number': int(item_number)
+            }
+        else:
+            ticket_id = int(x)
+
+        print 'x = {0} -> ticket_id = {1}'.format(x, ticket_id)
+
+        items.append(ticket_id)
+
+    return items
+#    return [int(x) for x in TICKET_RE.findall(string)]
 
 
 class CommitParser(object):
-    _ticket_reference = TICKET_PREFIX + '[0-9]+'
+    _ticket_reference = TICKET_PREFIX + TICKET_ID
     _ticket_command = (r'(?P<action>[A-Za-z]*) ?(task|issue|defect|bug|item|ticket|:)?.?'
-        '(?P<ticket>%s(?:(?:[, &]*|[ ,]+?and[ ]?)%s)*)' %
-        (_ticket_reference, _ticket_reference))
+        '(?P<ticket>%s(?:(?:[, &]*|[ ,]+?and[ ]?)%s)*)' % (_ticket_reference, _ticket_reference))
 
     _command_re = re.compile(_ticket_command, re.I | re.UNICODE)
-    _ticket_re = re.compile(TICKET_PREFIX + '([0-9]+)', re.I | re.UNICODE)
     _parsers = ['commands']
     _item_cmds = {'close': 'close_ticket',
                   'closed': 'close_ticket',
@@ -60,13 +77,20 @@ class CommitParser(object):
     def _parse_commands(self, message):
         cmd_groups = CommitParser._command_re.findall(message)
 
+        print cmd_groups
+
         commands = []
-        for cmd, junk, tkts in cmd_groups:
+
+        x = lambda bit: (bit and bit not in [':'])
+
+        for group in cmd_groups:
+            bits = filter(bool, group)
+            cmd, tkts = (bits[0], bits[1])
             try:
                 command = CommitParser._item_cmds[cmd.lower()]
                 tickets = items_mentioned(tkts)
                 for t in tickets:
-                    commands.append({command: int(t)})
+                    commands.append({command: t})
             except KeyError:
                 pass
 
